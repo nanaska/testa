f59ce36e5a1d32b9efe73a0f70b6bf555c57cd11
Denis Shakhov <fefe.d43efe@mail.ru>
2021-12-01 00:02:22 +0300

    1

---
+++ components/Hero.js
@@ -0,0 +1,16 @@
+import { motion } from "framer-motion"
+import photo from "./very-majestic-and-beautiful-landscape-with-mountains-in-banff-national-park-alberta-canada.jpg"
+import Image from "next/dist/client/image"
+
+function Hero() {
+    return (<>
+    
+        123
+    
+    
+    </>
+    
+    )
+}
+
+export default Hero
